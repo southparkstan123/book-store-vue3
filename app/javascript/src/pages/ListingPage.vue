45e4c5ac6a9e1f460cb74e5b0dbe590e106e248c
southparkstan123 <southparkstan123@gmail.com>
2023-08-18 10:55:51 +0800

    Listing page

--- app/javascript/src/pages/ListingPage.vue
+++ app/javascript/src/pages/ListingPage.vue
@@ -1,53 +1,63 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
     <Transition :appear="true" name="fade" mode="out-in">
-      <div v-if="!isLoading && data.length > 0">
-        <TableComponent :data="data" :fields="fields">
-          <template #caption>
-            {{ caption }}
-          </template>
-          <template #creator="{ item }">
-            {{ item.creator.username }}
-          </template>
-          <template #updater="{ item }">
-            {{ item.updater.username }}
-          </template>
-          <template #publisher="{ item }">
-            {{ item.name }}
-          </template>
-          <template #authors="{ item }">
-            {{ item.authors.map(author => author.name).join(", ") }}
-          </template>
-          <template #created_at="{ item }">
-            {{ moment(item.created_at).format('lll') }}
-          </template>
-          <template #updated_at="{ item }">
-            {{ moment(item.updated_at).format('lll') }}
-          </template>
-          <template #books="{ item }">
-            {{ item.books.map(book => book.name).join(", ") }}
-          </template>
-          <template #addition-header>
-            <th>Actions</th>
-          </template>
-          <template #addition-content="{ item }">
-            <td>
-              <button class="button success" @click="action('edit', item.id)">edit</button>
-              <button class="button danger" @click="action('delete', item.id)">delete</button>
-            </td>
-          </template>
-          <template #number="{ numberOfRecords }">
-            <span>{{ numberOfRecords }} {{ numberOfRecords > 1 ? 'records' : 'record' }} </span>
-          </template>
-        </TableComponent>
-      </div>
-      <div v-else>
-        <div v-if="!isLoading && isError">
-          <h1 class="text-center text-2xl text-red-500"></h1>Oops! Error occurs!
+      <div v-if="!isLoading">
+        <div v-if="!isError">
+          <div v-if="data.length > 0">
+            <TableComponent :data="data" :fields="fields">
+              <template #caption>
+                <router-link to="add">Add {{ category }}</router-link>
+              </template>
+              <template #creator="{ item }">
+                {{ item.creator.username }}
+              </template>
+              <template #updater="{ item }">
+                {{ item.updater.username }}
+              </template>
+              <template #publisher="{ item }">
+                {{ item.publisher.name }}
+              </template>
+              <template #authors="{ item }">
+                {{ item.authors.map(author => author.name).join(", ") }}
+              </template>
+              <template #created_at="{ item }">
+                {{ moment(item.created_at).format('lll') }}
+              </template>
+              <template #updated_at="{ item }">
+                {{ moment(item.updated_at).format('lll') }}
+              </template>
+              <template #books="{ item }">
+                {{ item.books.map(book => book.name).join(", ") }}
+              </template>
+              <template #addition-header>
+                <th>Actions</th>
+              </template>
+              <template #addition-content="{ item }">
+                <td>
+                  <button class="button success" @click="action('edit', item.id)">edit</button>
+                  <button class="button danger" @click="action('delete', item.id)">delete</button>
+                </td>
+              </template>
+              <template #number="{ numberOfRecords }">
+                <span>{{ numberOfRecords }} {{ numberOfRecords > 1 ? 'records' : 'record' }} </span>
+              </template>
+            </TableComponent>
+          </div>
+          <div v-else>
+            <div class="text-center">
+              <h1 class="text-2xl text-red-500">No {{ category }}</h1>
+              <router-link to="add">Add {{ category }}</router-link>
+            </div>
+          </div>
         </div>
         <div v-else>
-          <h1 class="text-center text-2xl text-red-500">Loading...</h1>
+          <div class="text-center">
+            <h1 class="text-2xl text-red-500">Oops! Error occurs.</h1>
+          </div>
         </div>
+      </div>
+      <div v-else>
+        <h1 class="text-center text-2xl text-red-500">Loading...</h1>
       </div>
     </Transition>
   </div>
@@ -100,7 +110,7 @@ const fetchRecords = async (module: Module) => {
 
 const action = async (type: ActionType, id: number) => {
   try {
-    if(type === 'edit') {
+    if (type === 'edit') {
       router.push({ path: `/${props.category}/${type}/${id}`, replace: true })
     } else {
       const confirm = await modalStore.open({
@@ -110,7 +120,7 @@ const action = async (type: ActionType, id: number) => {
         component: ''
       })
 
-      if(confirm) {
+      if (confirm) {
         await deleteRecordById(id, props.category)
         router.push(`/${props.category}/list`)
       }
@@ -137,16 +147,19 @@ watch(() => props.category, (newValue) => {
 
 <style scoped>
 .fade-enter {
-    opacity: 0;
+  opacity: 0;
 }
+
 .fade-enter-active {
-    transition: opacity 0.3s;
+  transition: opacity 0.3s;
 }
+
 .fade-leave {
-    opacity: 0;
+  opacity: 0;
 }
+
 .fade-leave-active {
-    transition: opacity 0.3s;
-    opacity: 0;
+  transition: opacity 0.3s;
+  opacity: 0;
 }
 </style>
