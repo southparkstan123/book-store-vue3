7805c43a00b8972cbf86e148b85f953dde4afa83
southparkstan123 <southparkstan123@gmail.com>
2023-08-18 13:05:50 +0800

    Apply on button component main page

--- app/javascript/src/pages/MainPage.vue
+++ app/javascript/src/pages/MainPage.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
     <div class="mx-auto">
-      <button class="button bg-green-400" @click="openModal">Open Modal</button>
+      <ButtonComponent
+        @buttonClicked="openModal"
+        :buttonType="'button'"
+        :textClass="'text-sm text-white'"
+        :backgroundClass="'bg-green-400 py-2 px-4'"
+      >
+        <template #text>
+          Open Modal
+        </template>
+      </ButtonComponent>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useModalStore } from '../store/modal'
+import ButtonComponent from '../components/inputs/ButtonComponent.vue';
 const modalStore = useModalStore()
 
 const openModal = () => {
