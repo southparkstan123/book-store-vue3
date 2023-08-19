c3b2fd851e80e87735cfb3301eeae197c443466c
southparkstan123 <southparkstan123@gmail.com>
2023-08-19 23:13:02 +0800

    input with debounce

--- app/javascript/src/pages/MainPage.vue
+++ app/javascript/src/pages/MainPage.vue
@@ -11,12 +11,26 @@
           Open Modal
         </template>
       </ButtonComponent>
+      <InputField 
+        :inputId="'test'"
+        :className="''"
+        :inputValue="message" 
+        :inputFieldClass="'block w-full mt-1'"
+        :inputType="'text'" 
+        :placeholder="'name'"
+        @changeValue="actionWithDebounce"
+      ></InputField>
+      {{ message }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import debounce from 'lodash.debounce'
+
 import { useModalStore } from '@/store/modal'
+import InputField from '@/components/inputs/InputField.vue';
 import ButtonComponent from '@/components/inputs/ButtonComponent.vue';
 const modalStore = useModalStore()
 
@@ -28,6 +42,12 @@ const openModal = () => {
     message: 'Welcome to Book-store on Vue3! This is Main Page.'
   })
 }
+
+const message = ref('')
+
+const actionWithDebounce = debounce((payload) => {
+  message.value = payload
+}, 1000)
 
 </script>
 
