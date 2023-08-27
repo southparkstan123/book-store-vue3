9d901c404d063977bcc0ba30a99375dadcf7e8e8
southparkstan123 <southparkstan123@gmail.com>
2023-08-27 15:56:09 +0800

    Define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,31 @@ module.exports = {
   theme: {
     fontFamily: {},
     extend: {
-    },
+      colors: {
+        'info': 'var(--color-info)',
+        'success': 'var(--color-success)',
+        'warning': 'var(--color-warning)',
+        'danger': 'var(--color-danger)',
+        'primary': 'var(--color-primary)',
+        'secondary': 'var(--color-secondary)',
+        'light': 'var(--color-light)',
+        'dark': 'var(--color-dark)',
+        'muted': 'var(--color-muted)',
+        'navbar': 'var(--color-navbar)',
+        'menu-brand': 'var(--color-menu-brand)',
+        'menu-item': 'var(--color-menu-item)',
+        'menu-button': 'var(--color-menu-button)',
+        'table': 'var(--color-table)',
+        'table-text': 'var(--color-table-text)',
+        'table-title-1': 'var(--color-table-title-1)',
+        'table-title-2': 'var(--color-table-title-2)',
+        'table-header': 'var(--color-table-header)',
+        'table-body-1': 'var(--color-table-body-1)',
+        'table-body-2': 'var(--color-table-body-2)',
+        'table-footer': 'var(--color-table-footer)',
+        'table-footer-text': 'var(--color-table-footer-text)'
+      },
+    }
   },
   corePlugins: {
     aspectRatio: false,
