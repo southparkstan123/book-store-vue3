e836547fb212a2acf6e6b4c369b2a454b8fa72bc
southparkstan123 <southparkstan123@gmail.com>
2023-08-31 17:39:15 +0800

    Add 'labelClass' props for LabelWrapper component

--- app/javascript/src/components/inputs/LabelWrapper.vue
+++ app/javascript/src/components/inputs/LabelWrapper.vue
@@ -1,16 +1,19 @@
 <template>
-  <label class="block" :for="forAttribute">
+  <label :class="labelClass" :for="forAttribute">
     <span :class="textClass">{{ labelText }}</span>
     <slot></slot>
   </label>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+const props = withDefaults(defineProps<{
   labelText: string;
   textClass: string;
   forAttribute: string;
-}>();
+  labelClass: string;
+}>(), {
+  labelClass: 'block'
+});
 </script>
 
 <style scoped></style>
