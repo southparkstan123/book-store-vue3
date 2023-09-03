a605eeed1257c82e2429347e78766b397ea90158
southparkstan123 <southparkstan123@gmail.com>
2023-09-03 14:34:24 +0800

    Modify the style of column filter

--- app/javascript/src/components/table/ColumnFilter.vue
+++ app/javascript/src/components/table/ColumnFilter.vue
@@ -1,35 +1,29 @@
 <template>
-  <div v-if="data" class="inline">
-    <InputField 
-      v-for="item in fields" 
-      :inputId="item.key" 
-      :className="'my-1'" 
-      :inputValue="item.key"
-      :inputFieldClass="'px-1 text-primary border-secondary focus:ring-0'" 
-      :inputName="'districts'"
-      :checked="item.isVisible"
-      @changeValue="({ checked, value }) => onChangeVisible(item, checked)"
+  <FieldsetWrapper 
+    :wrapperClass="'border border-solid border-info p-3'" 
+    :textClass="'text-sm text-info'"
+    :title="'Column Filter'"
+  >
+    <InputField v-for="item in fields" :inputId="item.key" :className="'my-1'" :inputValue="item.key"
+      :inputFieldClass="'px-1 text-primary border-secondary focus:ring-0'" :inputName="'districts'"
+      :checked="item.isVisible" @changeValue="({ checked, value }) => onChangeVisible(item, checked)"
       :inputType="'checkbox'">
       <template #label>
         <label :for="item.key" class="px-3">{{ item.key }}</label>
         <span>Label:</span>
-        <InputField 
-          :className="'my-1 inline'" 
-          :inputType="'text'" 
-          :placeholder="`Label of ${item.key}`"
-          :inputValue="item.label"
-          :isDisabled="item.isVisible === false"
-          @changeValue="(payload) => onChangeLabel(item, payload)"
-        ></InputField>
+        <InputField :className="'my-1 inline'" :inputType="'text'" :placeholder="`Label of ${item.key}`"
+          :inputValue="item.label" :isDisabled="item.isVisible === false"
+          @changeValue="(payload) => onChangeLabel(item, payload)"></InputField>
       </template>
     </InputField>
-  </div>
+  </FieldsetWrapper>
 </template>
 
 <script setup lang="ts">
 import { reactive, computed, onMounted } from 'vue';
 import type { TableItem, TableField } from "@/types/types";
 import InputField from "@/components/inputs/InputField.vue";
+import FieldsetWrapper from '../inputs/FieldsetWrapper.vue';
 
 type DataProps = {
   data: TableItem[] | undefined;
@@ -44,7 +38,7 @@ const props = withDefaults(defineProps<DataProps>(), {
 });
 
 const test = computed<Fields[]>(() => {
-  return (props.presetFields) ? props.presetFields.map((field) => { 
+  return (props.presetFields) ? props.presetFields.map((field) => {
     return {
       ...field,
       isVisible: true
@@ -56,13 +50,13 @@ const fields = reactive<Fields[]>(
   (props.data) ? Object.entries(props.data[0]).map(([_key]) => {
     const _test = test.value.find(item => item.key === _key);
     return {
-      key: _key, 
+      key: _key,
       label: _test ? _test.label : _key,
       isVisible: props.presetFields ? _test ? _test.isVisible : false : true
     }
-}): []);
+  }) : []);
 
-const emit = defineEmits<{e; 'onChangeColumn'}>()
+const emit = defineEmits<{ e; 'onChangeColumn' }>()
 
 const onChangeVisible = (item, checked) => {
   item.isVisible = checked;
@@ -74,12 +68,10 @@ const onChangeLabel = (item, label) => {
   emit('onChangeColumn', displayedFields.value)
 }
 
-const displayedFields = computed<TableField[]>(() => fields.filter(({ isVisible }) => isVisible).map(({ key, label}) => { return { key,  label} }))
+const displayedFields = computed<TableField[]>(() => fields.filter(({ isVisible }) => isVisible).map(({ key, label }) => { return { key, label } }))
 
 // Init
 emit('onChangeColumn', displayedFields.value)
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
