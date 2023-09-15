e8e1bb6ef5336b89e0db5ac799b5f21cb1d749d8
southparkstan123 <southparkstan123@gmail.com>
2023-09-16 01:56:06 +0800

    Move' Upload Images' to MainPage

--- app/javascript/src/components/forms/TemplateForm.vue
+++ app/javascript/src/components/forms/TemplateForm.vue
@@ -56,36 +56,6 @@
         </li>
       </ul>
     </FieldsetWrapper>
-
-    <LabelWrapper :forAttribute="'images'" :labelClass="'inline-block my-3 cursor-pointer bg-info py-2 px-4'"
-      :textClass="'text-sm text-white'" :labelText="'Upload Images'">
-      <InputField :inputId="'images'" :className="''" :inputValue="''" :inputFieldClass="'hidden'" :inputName="'images'"
-        :inputType="'file'" :isMultiple="true" @changeValue="onChangeFile" :accept="'image/*'">
-      </InputField>
-    </LabelWrapper>
-  </div>
-
-  <div v-if="imageData.length > 0 && imageData !== undefined" :style="'height: 200px'" class="overflow-y-scroll">
-    <CardList :data="(imageData as ImageFile[])">
-      <template v-slot="{ item, index }">
-        <CardItem :wrapperClass="(index % 2 === 0) ? 'bg-table-body-1' : 'bg-table-body-2'" :item="(item as ImageFile)"
-          :class="'text-table-text'">
-          <template #close-button>
-            <div :class="'float-right cursor-pointer p-1'" @click="deleteImage(index)">x</div>
-          </template>
-          <template v-slot="{ src, type, createdAt, size }">
-            <div class="flex justify-center">
-              <img :src="(src as string)" width="150" height="150" />
-            </div>
-            <ul>
-              <li>Type: {{ type }}</li>
-              <li>Size: {{ (size as number / 1024 / 1024).toPrecision(3) + 'MB' }}</li>
-              <li>Create At: {{ moment(createdAt as number).fromNow() }}</li>
-            </ul>
-          </template>
-        </CardItem>
-      </template>
-    </CardList>
   </div>
 
   <div class="float-right">
@@ -104,16 +74,6 @@ import FieldsetWrapper from '../inputs/FieldsetWrapper.vue';
 import LabelWrapper from '../inputs/LabelWrapper.vue';
 import ButtonComponent from '../inputs/ButtonComponent.vue';
 import DropdownMenu from '../dropdowns/DropdownMenu.vue';
-import ToggleSwitch from '../inputs/ToggleSwitch.vue';
-
-import CardList from "@/components/card/CardList.vue";
-import CardItem from "@/components/card/CardItem.vue";
-
-// Upload Files
-import moment from 'moment';
-import type { ImageFile } from '@/types/types';
-import { useUploadFile } from '@/hooks/useUploadFile';
-const { imageData, onChangeFile, deleteImage } = useUploadFile();
 
 // Modal
 import { useModalStore } from '@/store/modal';
--- app/javascript/src/pages/MainPage.vue
+++ app/javascript/src/pages/MainPage.vue
@@ -1,14 +1,49 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
     <div class="mx-auto">
-      <ButtonComponent
-        @buttonClicked="openModal"
-        :buttonType="'button'"
-        :textClass="'text-sm text-white'"
-        :backgroundClass="'bg-info py-2 px-4 disabled:opacity-25'"
-      >
-        <template #text> Open Modal </template>
-      </ButtonComponent>
+      <div class="inline">
+        <div>
+          <ButtonComponent
+            @buttonClicked="openModal"
+            :buttonType="'button'"
+            :textClass="'text-sm text-white'"
+            :backgroundClass="'bg-info py-2 px-4 disabled:opacity-25'"
+          >
+            <template #text> Open Modal </template>
+          </ButtonComponent>
+        </div>
+        <div>
+          <LabelWrapper :forAttribute="'images'" :labelClass="'inline-block my-3 cursor-pointer bg-info py-2 px-4'"
+            :textClass="'text-sm text-white'" :labelText="'Upload Images'">
+            <InputField :inputId="'images'" :className="''" :inputValue="''" :inputFieldClass="'hidden'" :inputName="'images'"
+              :inputType="'file'" :isMultiple="true" @changeValue="onChangeFile" :accept="'image/*'">
+            </InputField>
+          </LabelWrapper>
+        </div>
+      </div>
+
+      <div v-if="imageData.length > 0 && imageData !== undefined" :style="'height: 200px'" class="overflow-y-scroll">
+        <CardList :data="(imageData as ImageFile[])">
+          <template v-slot="{ item, index }">
+            <CardItem :wrapperClass="(index % 2 === 0) ? 'bg-table-body-1' : 'bg-table-body-2'" :item="(item as ImageFile)"
+              :class="'text-table-text'">
+              <template #close-button>
+                <div :class="'float-right cursor-pointer p-1'" @click="deleteImage(index)">x</div>
+              </template>
+              <template v-slot="{ src, type, createdAt, size }">
+                <div class="flex justify-center">
+                  <img :src="(src as string)" width="150" height="150" />
+                </div>
+                <ul>
+                  <li>Type: {{ type }}</li>
+                  <li>Size: {{ (size as number / 1024 / 1024).toPrecision(3) + 'MB' }}</li>
+                  <li>Create At: {{ moment(createdAt as number).fromNow() }}</li>
+                </ul>
+              </template>
+            </CardItem>
+          </template>
+        </CardList>
+      </div>
     </div>
   </div>
 </template>
@@ -27,6 +62,18 @@ const openModal = () => {
     props: undefined
   });
 };
+
+import LabelWrapper from '@/components/inputs/LabelWrapper.vue';
+import InputField from "@/components/inputs/InputField.vue";
+
+import CardList from "@/components/card/CardList.vue";
+import CardItem from "@/components/card/CardItem.vue";
+
+// Upload Files
+import moment from 'moment';
+import type { ImageFile } from '@/types/types';
+import { useUploadFile } from '@/hooks/useUploadFile';
+const { imageData, onChangeFile, deleteImage } = useUploadFile();
 
 </script>
 
