dfe15655b748a39d5fdec5ad138fd5b063eaaaa1
southparkstan123 <southparkstan123@gmail.com>
2023-09-16 12:37:05 +0800

    Modify style of MainPage

--- app/javascript/src/pages/MainPage.vue
+++ app/javascript/src/pages/MainPage.vue
@@ -2,47 +2,58 @@
   <div class="min-h-screen flex items-center justify-center">
     <div class="mx-auto">
       <div class="inline">
-        <div>
-          <ButtonComponent
-            @buttonClicked="openModal"
-            :buttonType="'button'"
-            :textClass="'text-sm text-white'"
-            :backgroundClass="'bg-info py-2 px-4 disabled:opacity-25'"
-          >
-            <template #text> Open Modal </template>
-          </ButtonComponent>
-        </div>
-        <div>
-          <LabelWrapper :forAttribute="'images'" :labelClass="'inline-block my-3 cursor-pointer bg-info py-2 px-4'"
-            :textClass="'text-sm text-white'" :labelText="'Upload Images'">
-            <InputField :inputId="'images'" :className="''" :inputValue="''" :inputFieldClass="'hidden'" :inputName="'images'"
-              :inputType="'file'" :isMultiple="true" @changeValue="onChangeFile" :accept="'image/*'">
-            </InputField>
-          </LabelWrapper>
-        </div>
+        <ButtonComponent @buttonClicked="openModal" :buttonType="'button'" :textClass="'text-sm text-white'"
+          :backgroundClass="'bg-info m-1 py-2 px-4 disabled:opacity-25'">
+          <template #text> Open Modal </template>
+        </ButtonComponent>
+
+        <LabelWrapper :forAttribute="'images'" :labelClass="'inline-block m-1 cursor-pointer bg-info py-2 px-4'"
+          :textClass="'text-sm text-white'" :labelText="'Upload Images'">
+          <InputField :inputId="'images'" :className="''" :inputValue="''" :inputFieldClass="'hidden'"
+            :inputName="'images'" :inputType="'file'" :isMultiple="true" @changeValue="onChangeFile" :accept="'image/*'">
+          </InputField>
+        </LabelWrapper>
       </div>
 
-      <div v-if="imageData.length > 0 && imageData !== undefined" :style="'height: 200px'" class="overflow-y-scroll">
-        <CardList :data="(imageData as ImageFile[])">
+      <div v-if="imageData.length > 0 && imageData !== undefined" :style="'height: 400px;min-width:600px;'"
+        class="overflow-scroll">
+        <CardList :data="(imageData as ImageFile[])" :wrapperClass="'p-3 grid w-full gap-6 lg:grid-cols-4 md:grid-cols-2'">
           <template v-slot="{ item, index }">
-            <CardItem :wrapperClass="(index % 2 === 0) ? 'bg-table-body-1' : 'bg-table-body-2'" :item="(item as ImageFile)"
-              :class="'text-table-text'">
+            <CardItem :wrapperClass="''" :item="(item as ImageFile)" class="text-white text-sm">
               <template #close-button>
-                <div :class="'float-right cursor-pointer p-1'" @click="deleteImage(index)">x</div>
-              </template>
-              <template v-slot="{ src, type, createdAt, size }">
-                <div class="flex justify-center">
-                  <img :src="(src as string)" width="150" height="150" />
+                <div 
+                  :class="'float-right cursor-pointer p-3 text-lg'" 
+                  @click="deleteImage(index)"
+                >
+                  x
                 </div>
-                <ul>
-                  <li>Type: {{ type }}</li>
-                  <li>Size: {{ (size as number / 1024 / 1024).toPrecision(3) + 'MB' }}</li>
-                  <li>Create At: {{ moment(createdAt as number).fromNow() }}</li>
-                </ul>
+              </template>
+              <template v-slot="{ name, src, type, createdAt, size }">
+                <div :style="`
+                  background-image:url(${src});
+                  height: 200px;
+                  min-width:200px;
+                  background-position: 50% 50%;
+                  background-size: cover;
+                  background-repeat: no-repeat;`"
+                >
+                  <div 
+                    class="p-2" 
+                    :style="`background: linear-gradient(to bottom,rgba(0,0,0,.7) 30%,rgba(255,255,255,0))`"
+                  >
+                    <div>{{ name }}</div>
+                    <div>Type: {{ type }}</div>
+                    <div>Size: {{ (size as number / 1024 / 1024).toPrecision(3) + 'MB' }}</div>
+                    <div>Create At: {{ moment(createdAt as number).fromNow() }}</div>
+                  </div>
+                </div>
               </template>
             </CardItem>
           </template>
         </CardList>
+      </div>
+      <div v-else class="flex items-center justify-center" :style="'height: 400px;min-width:600px;'">
+        <h1 class="text-2xl text-primary">No Images</h1>
       </div>
     </div>
   </div>
