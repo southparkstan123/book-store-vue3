609194665a1ac0583acb0ffc1f33556c11f8b849
southparkstan123 <southparkstan123@gmail.com>
2023-09-16 12:31:56 +0800

    Add wrapperClass props to CartList component

--- app/javascript/src/components/card/CardList.vue
+++ app/javascript/src/components/card/CardList.vue
@@ -1,11 +1,17 @@
 <template>
-  <div v-for="(item, index) in data" v-if="data && data.length > 0"> 
-    <slot v-bind="{ item , index }"></slot>
+  <div :class="wrapperClass">
+    <div v-for="(item, index) in data" v-if="data && data.length > 0"> 
+      <slot v-bind="{ item , index }"></slot>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps<{data: unknown[]}>()
+const props = withDefaults(defineProps<{data: unknown[]; wrapperClass: string}>(),{
+  data: () => [],
+  wrapperClass: ''
+})
+
 </script>
 
 <style scoped>
