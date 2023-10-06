791ad5ff36db05b66130020eac08940449434969
southparkstan123 <southparkstan123@gmail.com>
2023-10-06 14:44:11 +0800

    Migrate to Postgresql from MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2023_08_15_115115) do
 
-  create_table "authors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.bigint "creator_id"
@@ -23,14 +26,14 @@ ActiveRecord::Schema.define(version: 2023_08_15_115115) do
     t.index ["updater_id"], name: "index_authors_on_updater_id"
   end
 
-  create_table "authors_books", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "authors_books", id: false, force: :cascade do |t|
     t.bigint "author_id"
     t.bigint "book_id"
     t.index ["author_id"], name: "index_authors_books_on_author_id"
     t.index ["book_id"], name: "index_authors_books_on_book_id"
   end
 
-  create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "name"
     t.text "abstract"
     t.decimal "price", precision: 5, scale: 2
@@ -44,7 +47,7 @@ ActiveRecord::Schema.define(version: 2023_08_15_115115) do
     t.index ["updater_id"], name: "index_books_on_updater_id"
   end
 
-  create_table "publishers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "publishers", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.bigint "creator_id"
@@ -55,7 +58,7 @@ ActiveRecord::Schema.define(version: 2023_08_15_115115) do
     t.index ["updater_id"], name: "index_publishers_on_updater_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password_digest"
