a7cca305ae877e7267065d107742022ee19bbe72
southparkstan123 <southparkstan123@gmail.com>
2023-10-31 11:07:31 +0800

    Set margin top and bottom for editpage

--- app/javascript/src/pages/EditPage.vue
+++ app/javascript/src/pages/EditPage.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen flex items-center justify-center">
     <Transition :appear="true" name="fade">
       <component
-        class="overflow-scroll p-2"
+        class="overflow-scroll p-2 my-12"
         :is="formComponent"
         :id="id"
         @formChanged="formChanged"
