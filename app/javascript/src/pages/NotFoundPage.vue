af853eb80fc73a50dbda2ce13c13808260e1b285
southparkstan123 <southparkstan123@gmail.com>
2023-12-08 14:03:49 +0800

    Add Dynamic text for not found page

--- app/javascript/src/pages/NotFoundPage.vue
+++ app/javascript/src/pages/NotFoundPage.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
-    <div class="mx-auto">Oops! No Found!</div>
+    <LoadingComponent class="text-3xl text-primary" :text="'Oops! No Found!'" :animationType="'wavy'"/>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoadingComponent from '@/components/loading/LoadingComponent.vue';
+</script>
 
 <style scoped></style>
