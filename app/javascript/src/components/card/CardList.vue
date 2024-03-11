515c6e8e52f9b4149531d9adf4a4b793a17f667c
southparkstan123 <southparkstan123@gmail.com>
2024-03-11 12:10:13 +0800

    Add TransitionGroup

--- app/javascript/src/components/card/CardList.vue
+++ app/javascript/src/components/card/CardList.vue
@@ -1,19 +1,36 @@
 <template>
-  <div :class="wrapperClass">
-    <div v-for="(item, index) in data" v-if="data && data.length > 0">
+  <TransitionGroup name="list" tag="div" :class="wrapperClass" mode="in-out">
+    <div v-for="(item, index) in data" :key="item[key]" v-if="data && data.length > 0">
       <slot v-bind="{ item, index }"></slot>
     </div>
-  </div>
+  </TransitionGroup>
 </template>
 
 <script lang="ts" setup>
 const props = withDefaults(
-  defineProps<{ data: unknown[]; wrapperClass: string }>(),
+  defineProps<{ data: unknown[]; wrapperClass: string, key: string }>(),
   {
     data: () => [],
     wrapperClass: "",
+    key: 'name'
   },
 );
 </script>
 
-<style scoped></style>
+<style scoped>
+.list-move,
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter-from {
+  transform: translateY(-10%);
+  opacity: 0;
+}
+
+.list-leave-to {
+  transform: translateY(10%);
+  opacity: 0;
+}
+</style>
