0c54dff3019848c3f7b97f6c7d1890e81171dd2f
southparkstan123 <southparkstan123@gmail.com>
2024-03-18 10:43:39 +0800

    Add Dashboard page

---
+++ app/javascript/src/pages/DashboardPage.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="min-h-screen flex justify-center">
+    <div v-if="isLoading === false" class="mt-16 w-full mx-2">
+      <h1 class="text-3xl my-2 text-primary">
+        <font-awesome-icon icon="fa-solid fa-gauge" class="mr-3"/>Dashboard
+      </h1>
+      <div class="flex">
+        <div v-for="(num, key) in totalNumbers" class="bg-dark p-6 rounded-xl mx-1">
+          <span class="text-info text-md">
+            <font-awesome-icon :icon="`fa-solid fa-${icon(key)}`" /> {{ key.toUpperCase() }} :
+          </span>
+          <span class="text-light text-5xl">
+            {{ num }}
+          </span>
+          <router-link class="block text-success" :to="`/${key}/list`">
+            More >>
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <div class="flex items-center justify-center" v-else>
+      <LoadingComponent
+        class="text-2xl text-primary"
+        :text="'Loading...'"
+        :animationType="'fade-in-zoom-in'"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { getSummary } from '@/services/CRUDServices';
+import { useMessageStore } from '@/store/message';
+import { onMounted, ref } from 'vue';
+import LoadingComponent from '@/components/loading/LoadingComponent.vue';
+import type { ModuleType } from '@/types/types';
+
+const messageStore = useMessageStore();
+
+const totalNumbers = ref<{
+  book: number,
+  author: number,
+  publisher: number
+}>({
+  book: 0,
+  author: 0,
+  publisher: 0,
+});
+
+const isLoading = ref<boolean>(true);
+
+const icon = (moduleType: ModuleType) => {
+  switch (moduleType) {
+    case 'book':
+      return 'book';
+    case 'author':
+      return 'at';
+    case 'publisher':
+      return 'building';
+  }
+}
+
+onMounted(async () => {
+  try {
+    isLoading.value = true;
+    const list = {};
+    for await (const response of getSummary()) {
+      Object.assign(list, response);
+    }
+    totalNumbers.value = list;
+  } catch (error) {
+    messageStore.push({
+      type: "error",
+      content: error
+    });
+  } finally {
+    isLoading.value = false;
+  }
+})
+</script>
+
+<style scoped></style>
